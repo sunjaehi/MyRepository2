fdf6cafc220cf94226f46383f8138af416893215
Lee Sunjae <sunjae7944@naver.com>
2023-12-23 21:25:18 +0900

    Initial Version

---
+++ GitHW3/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sunja
+ *
+ */
+module GitHW3 {
+}
---
+++ GitHW3/src/mygit2/MyClass2.java
@@ -0,0 +1,8 @@
+package mygit2;
+
+public class MyClass2 {
+	public static void main(String args[]) {
+		System.out.println("Hello Git");
+	}
+
+}
